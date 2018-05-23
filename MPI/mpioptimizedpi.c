48891968d76398f3043f32132e39f70bc95cd5fa
ohadkatz <ohadkatz@buffalo.edu>
2018-05-23 11:49:38 -0400

    Added new functions, seg fault on MPI_REDUCE, need to beautify

--- MPI/mpioptimizedpi.c
+++ MPI/mpioptimizedpi.c
@@ -4,40 +4,98 @@
 #include <stdio.h>
 
 
-double PiCalc(N) {
-    int n= 10000000000;
-    double PI25DT= 3.141592653589793238462643;
-    double mypi, pi , h , sum , x;
-    start= clock();
-    MPI_Comm_size(MPI_COMM_WORLD, N);
+/*
+*
+* MPI PI COMPARE FUNCTIONS
+* Author: Ohad Katz
+* Date: 5/23/18
+* 
+*/
+
+double ScalarPiCalc(int N) {
+    int myid, i, numprocs;
+    
+    double ScalTime, mypi, pi , h , sum , x;
+    clock_t ScalStart= clock();
+    /*Determine Size of Group */
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    /*Determine Rank of Calling Process */
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    h= 1.0/ (double) n;
+
+    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    h= 1.0/ (double) N;
     sum = 0.0;
-    for (i=myid+1; i<=N; i++){
-        x= h* ((double)i-.5);
+    printf("Scalar Pi Calc\n");
+    /*INCREMENTS BY NUMBER OF PROCESSES EACH TIME*/
+    /*CALCULATE INTEGRAL*/
+    for (i=myid+1; i<=N; i+=numprocs){
+        x= h* ((double)i+.5);
         sum += 4.0/(1.0+ x*x);
     }
     mypi= h * sum;
+    /*Reduces values into single value, pi*/
     MPI_REDUCE(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (myid==0){
+        ScalTime= ((double) (clock()-ScalStart)/CLOCKS_PER_SEC);
+
+        printf("Scalar pi is approx. %.16f , Error is %.16f\n", pi, fabs(pi-PI25DT));
+        printf("Time to run Scalar Pi Calculation= %f\n", ScalTime);
+
+    }
+    return pi;
 }
 
 
-int main(argc,argv, Npoints, Nrepeat){
-    int arg;
-    int *argv[];
-    int Npoints;
-    int Nrepeat;
-    int arr[Npoints];
-    MPI_init(&argc, &argv);
+double VectorPiCalc(int N) {
+    int myid, i, numprocs;
+    double PI25DT= 3.141592653589793238462643;
+    double VecTime, mypi, pi , h , sum , x;
+    clock_t VecStart= clock();
+    /*Determine Size of Group */
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    /*Determine Rank of Calling Process */
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     
-    for (i=0 ; i <= Nrepeat; i++){
-        start= clock()
-        double pi = PiCalc(Npoints);
-        cpu_time= ((double) (clock()-start)/CLOCKS_PER_SEC);
-        printf("At iteration: %d, we get pi = %f , with %d processes,  in %f time! ", i, pi, Npoints, cpu_time);
+    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    h= 1.0/ (double) N;
+    sum = 0.0;
+    printf("Vector Pi Calc\n");
+    /*INCREMENTS BY ONE EACH TIME*/
+    for (i=0; i<N; i++){
+        printf("in vector pi for loop\n");
+        x= h* ((double)i+.5);
+        sum += 4.0/(1.0+ x*x);
     }
+    mypi= h * sum;
 
+    /*Reduces values into single value, pi*/
+    MPI_REDUCE(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (myid==0){
+        VecTime = ((double) (clock()-VecStart)/CLOCKS_PER_SEC);
+        printf("Vector Pi Calculation is approx. %.16f , Error is %.16f\n", pi, fabs(pi-PI25DT));
+        printf("Time to Run Vector Pi Calculation=%f\n", VecTime);
+    }
+    return pi;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int Npoints,Nrepeat, myid, i,numprocs;
+    double cpu_time;
+    printf("Please select # of Repetitions followed by # of Points:");
+    scanf("%d%d", &Nrepeat, &Npoints);
+    MPI_Init(&argc, &argv);
+
+    for (i=0 ; i <= Nrepeat+1; i++)
+    {
+        printf("Iteration #%d\n",i);
+        ScalarPiCalc(Npoints);
+        VectorPiCalc(Npoints);
+    }
     MPI_Finalize();
     return 0;
 }
+
